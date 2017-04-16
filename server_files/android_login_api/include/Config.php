eb2d774e08cb7a67a03f5aa2fc9e48715557e0eb
Sharavi95 <sharavi.ravi@live.com>
2017-04-16 15:50:21 +0400

    Add files via upload

---
+++ server_files/android_login_api/include/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "projects_appusr9");
+define("DB_PASSWORD", "kagoMO0@(~[&");
+define("DB_DATABASE", "projects_app1");
+?>
